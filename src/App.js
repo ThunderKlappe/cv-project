b3caf93b07ddcc3109f4826f2babbafa008fb677
ThunderKlappe <timschley@gmail.com>
2022-03-18 14:21:25 -0700

    Header added

--- src/App.js
+++ src/App.js
@@ -1,9 +1,22 @@
-import { Component } from "react";
+/* eslint-disable no-useless-constructor */
+import React, { Component } from "react";
+import Header from "./components/Header";
+import "./components/styles/App.css";
 
-class App extends Component() {
-  return (
-    
-  );
+class App extends Component {
+    constructor(props) {
+        super(props);
+    }
+    render() {
+        return (
+            <div id="main">
+                <Header id="header" />
+                {/* <PersonalInfo />
+                <JobHistory />
+                <Education /> */}
+            </div>
+        );
+    }
 }
 
 export default App;
---
+++ src/components/Header.js
@@ -0,0 +1,11 @@
+/* eslint-disable no-useless-constructor */
+import React, { Component } from "react";
+import "./styles/Header.css";
+
+class Header extends Component {
+    render() {
+        return <div id="header">CV Creator</div>;
+    }
+}
+
+export default Header;
